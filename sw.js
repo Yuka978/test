08f3b7546dc6a81eebf0a36c8e4661fcd60345b3
Yuka978 <minayuka73910@icloud.com>
2025-01-06 11:55:27 +0900

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,44 @@
+const cacheName = 'calculator-v1';
+const filesToCache = [
+    '/',
+    '/index.html',
+    '/styles.css',
+    '/script.js',
+    '/manifest.json',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png'
+];
+
+// インストール時にキャッシュするファイルを指定
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(filesToCache);
+        })
+    );
+});
+
+// ネットワークのフェッチリクエストをキャッシュで対応
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((cachedResponse) => {
+            return cachedResponse || fetch(event.request);
+        })
+    );
+});
+
+// サービスワーカーの更新時に古いキャッシュを削除
+self.addEventListener('activate', (event) => {
+    const cacheWhitelist = [cacheName];
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cache) => {
+                    if (!cacheWhitelist.includes(cache)) {
+                        return caches.delete(cache);
+                    }
+                })
+            );
+        })
+    );
+});
